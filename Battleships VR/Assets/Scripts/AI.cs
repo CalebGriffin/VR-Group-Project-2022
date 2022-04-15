bb1bf7343e89644a029ce10f26a4da8c279fff64
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-04-15 12:01:19 +0100

    Created AI.cs
    Has a board and boats, can be hit

---
+++ Battleships VR/Assets/Scripts/AI.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public GameObject shipCube;
+    public GameObject borderCube;
+
+    private Board board = new Board(10);
+
+    private List<Boat> boats = new List<Boat>();
+
+    private List<int> positionGuesses = new List<int>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        string positionGuessesString = "";
+        foreach(int i in board.Matrix)
+        {
+            positionGuesses.Add(i);
+            positionGuessesString += i + ", ";
+        }
+        Debug.Log(positionGuessesString);
+        
+        boats.Add(new Boat(board, "Carrier", 5));
+        boats.Add(new Boat(board, "Battleship", 4));
+        boats.Add(new Boat(board, "Cruiser", 3));
+        boats.Add(new Boat(board, "Submarine", 3));
+        boats.Add(new Boat(board, "Destroyer", 2));
+
+        string boatPositions = "";
+        foreach (Boat boat in boats)
+        {
+            Debug.Log(boat.Name + ":");
+            foreach (int position in boat.Positions)
+            {
+                boatPositions += position + ", ";
+            }
+            Debug.Log(boatPositions);
+            boatPositions = "";
+        }
+
+        string boatPositionsWithBorders = "";
+        foreach (int position in board.currentBoatPositionsWithBorders)
+        {
+            boatPositionsWithBorders += position + ", ";
+        }
+        Debug.Log(boatPositionsWithBorders);
+    }
+
+    private void ShotFired(int position)
+    {
+        bool hit = false;
+
+        foreach (Boat boat in boats)
+        {
+            if (boat.HitCheck(position))
+            {
+                hit = true;
+                // Enable the object on that position with the 'hit' object
+                Debug.Log("Hit!");
+                if (boat.SunkCheck())
+                {
+                    Debug.Log("Sunk!");
+                    int[] positionsAround = boat.Sunk(this.board);
+
+                    foreach (int positionAround in positionsAround)
+                    {
+                        if (positionAround != 0)
+                        {
+                            // Enable the object on that position with the 'miss' object
+                        }
+                    }
+                    WinCheck();
+                }
+                break;
+            }
+        }
+
+        if (!hit)
+        {
+            // Enable the object on that position with the 'miss' object
+            Debug.Log("Miss!");
+        }
+    }
+
+    private void WinCheck()
+    {
+        bool win = true;
+
+        foreach (Boat boat in boats)
+        {
+            if (!boat.SunkCheck())
+            {
+                win = false;
+                break;
+            }
+        }
+
+        if (win)
+        {
+            // Call a GameOver method to end the game because the player has won
+            Debug.Log("You win!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
