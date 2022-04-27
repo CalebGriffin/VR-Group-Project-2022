38c851ce629ff6e16dc0705b1f7464e63393c376
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-04-27 15:28:24 +0100

    Created a Simple Setup Script

---
+++ Battleships VR/Assets/Testing Stuff/MiniBoardSetup.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TESTING
+public class MiniBoardSetup : MonoBehaviour
+{
+    public GameObject boardParent;
+
+    public GameObject cubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Setup")]
+    public void Setup()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                GameObject go = Instantiate(cubePrefab, boardParent.transform.position + new Vector3(i * boardParent.transform.localScale.x, 0, j * boardParent.transform.localScale.z), Quaternion.identity, boardParent.transform);
+                go.name = "Piece " + i + "," + j;
+            }
+        }
+    }
+}
