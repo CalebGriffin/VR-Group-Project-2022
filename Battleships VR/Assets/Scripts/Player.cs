7e45a14e136cc4c2f38626310be30f32f57efc23
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-04-27 15:32:21 +0100

    Created Player.cs
    The player has a board, a list of boats and began adding some of the methods

---
+++ Battleships VR/Assets/Scripts/Player.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Board board = new Board(10);
+    public Board Board { get { return board; } }
+
+    private List<Boat> boats = new List<Boat>();
+    public List<Boat> Boats { get { return boats; } }
+
+    private List<(int Position, bool Hit, bool Sunk)> previousGuesses = new List<(int Position, bool Hit, bool Sunk)>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public (int, bool, bool) ShotFired(int position)
+    {
+        // Theses are the return values
+        bool hit = false;
+        bool sunk = false;
+
+        // Foreach of the boats in the boat list, if they have been hit then set the bool to true and check if they have been sunk then set the bool to true
+        foreach (Boat boat in boats)
+        {
+            if (boat.HitCheck(position))
+            {
+                hit = true;
+
+                // Enable the hit object at the position in the sea and on the mini board
+
+                if (boat.SunkCheck())
+                {
+                    sunk = true;
+
+                    // Get all of the points around the boat and place miss objects around them because the boats can't be there
+                    int[] positionsAround = boat.Sunk(this.board);
+                    foreach (int positionAround in positionsAround)
+                    {
+                        if (positionAround != 0)
+                        {
+                            // Enable the miss object at the position in the sea and on the mini board
+                        }
+                    }
+
+                    WinCheck();
+                }
+
+                break;
+            }
+        }
+
+        if (!hit)
+        {
+            // Enable the miss object at the position in the sea and on the mini board
+        }
+
+        return ValueTuple.Create(position, hit, sunk);
+    }
+
+    private void WinCheck()
+    {
+        bool win = true;
+
+        foreach (Boat boat in boats)
+        {
+            if (!boat.SunkCheck())
+            {
+                win = false;
+                break;
+            }
+        }
+
+        if (win)
+        {
+            // Call a GameOver method to end the game because the AI has won
+        }
+    }
+}
