f63fa201cd9e08e9fc94d7a33603e4579e1800e3
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-05-27 15:50:20 +0100

    Disabled the Main stuff in Dan's scene and Adjusted the Mats
    They can now be loaded together correctly

--- Battleships VR/Assets/Scripts/SceneController.cs
+++ Battleships VR/Assets/Scripts/SceneController.cs
@@ -8,7 +8,7 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadAllScenes();
     }
 
     // Update is called once per frame
