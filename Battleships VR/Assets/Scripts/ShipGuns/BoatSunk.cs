774ba1408e39817337c426a65923ca95a339129a
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2022-06-01 09:26:19 +0100

    All the boat parts now move with the boat when sinking
    Each part of the boat such as the fire will now be turned off when they hit the water and move with the boat

--- Battleships VR/Assets/Scripts/ShipGuns/BoatSunk.cs
+++ Battleships VR/Assets/Scripts/ShipGuns/BoatSunk.cs
@@ -6,8 +6,21 @@ public class BoatSunk : MonoBehaviour
 {
     [SerializeField] private AnimationCurve curve;
     [SerializeField] private float submergeDistance = -500f;
+    [SerializeField] private GameObject[] shipParts;
     public void SinkShip()
     {
+        foreach(GameObject obj in shipParts)
+        {
+            //Move the all the ship parts along with the model when it is sinking
+            obj.transform.parent = gameObject.transform;
+            StartCoroutine(TurnOffFire(obj));
+        }
         LeanTween.moveLocalY(gameObject, submergeDistance, 10f).setEase(curve);
+    }
+    private IEnumerator TurnOffFire(GameObject obj)
+    {
+        //Come on, fire can't burn underwater :D
+        yield return new WaitForSeconds(0.5f);
+        obj.SetActive(false);
     }
 }
